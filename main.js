eedd4e49efb6d8ea9147c5297579f58497957814
Samuel Sena <92332763+Samael7735@users.noreply.github.com>
2022-09-03 16:59:43 -0300

    Adicionando as Funções para calcular valores

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-const adicionar = document.getElementById('adicionar')
-const cancelar = document.getElementById('cancelar')
-const salvar = document.getElementById('salvar')
-
 let modal = document.getElementById('modal')
 
 function fechar(){
@@ -10,3 +6,106 @@ function fechar(){
 function abrir(){
   modal.classList.add('active')
 }
+//registros
+const transactions =[
+  {
+    id:0,
+    description: 'Luz',
+    amount: -50000,
+    date: '03/09/2022'
+  },
+  {
+    id:1,
+    description: 'Website',
+    amount: 20000,
+    date: '03/09/2022'
+  },
+  {
+    id:2,
+    description: 'Website',
+    amount: 20000,
+    date: '03/09/2022'
+  },
+  {
+    id:3,
+    description: 'Internet',
+    amount: -50000,
+    date: '03/09/2022'
+  }
+]
+//funcionalidades
+const transaction = {
+  incomes(){
+    let income = 0;
+    transactions.forEach(transaction=>{
+      if(transaction.amount > 0){
+        income += transaction.amount
+      }
+    })
+    return income;
+  },
+  expenses(){
+    let expense = 0;
+    transactions.forEach(transaction=>{
+      if(transaction.amount < 0){
+        expense += transaction.amount;
+      }
+    })
+    return expense;
+  },
+  total(){
+    return transaction.incomes() + transaction.expenses();
+  }
+}
+//utilidades
+const Utils = {
+  formatCurrency(value){
+    const signal = Number(value) < 0 ? "-" : ""
+    value = String(value).replace(/\D/g, "")
+    value = Number(value) / 100
+
+    value = value.toLocaleString("pt-BR", {
+      style: "currency",
+      currency : "BRL"
+    }) 
+    return (signal + value)
+  }
+}
+//DOM
+const dom ={
+  transactionContainer : document.querySelector('#data_table tbody'),
+
+  addTransaction(transaction, index){
+    const tr = document.createElement('tr')
+    tr.innerHTML = dom.innerHTMLTransaction(transaction)
+    
+    dom.transactionContainer.appendChild(tr)
+  },
+  innerHTMLTransaction(transaction){
+    const Cssclas = transaction.amount > 0 ? "income" : "expense"
+    const amount = Utils.formatCurrency(transaction.amount)
+    const html = `
+      <tr>
+        <td class="description">${transaction.description}</td>
+        <td class="${Cssclas}">${amount}</td>
+        <td class="date">${transaction.date}</td>
+        <td>
+          <img src="/assets/minus.svg" alt="remover Transação">
+        </td>
+      </tr>
+    `
+    return html
+  },
+
+  updateBalance(){
+    document.getElementById('incomeDisplay').innerHTML = Utils.formatCurrency(transaction.incomes())
+    document.getElementById('expenseDisplay').innerHTML = Utils.formatCurrency(transaction.expenses())
+    document.getElementById('totalDisplay').innerHTML = Utils.formatCurrency(transaction.total())
+  }
+}
+
+transactions.forEach(function(transaction){
+  dom.addTransaction(transaction)
+})
+
+dom.updateBalance()
